be5a83e3d9da8efcd083dcfbb5b8900a6efed927
unknown <996natam@gmail.com>
2026-06-04 00:26:37 +0200

    Order products tests - added test that user can only order product with valid card info

--- src/test/java/Tests/OrderProductTest.java
+++ src/test/java/Tests/OrderProductTest.java
@@ -1,6 +1,7 @@
 package Tests;
 
 import Base.BaseTest;
+import Base.ExcelReader;
 import Pages.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -16,7 +17,7 @@ import java.time.Duration;
 public class OrderProductTest extends BaseTest {
 
     @BeforeMethod
-    public void pageSetUp() {
+    public void pageSetUp() throws Exception {
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
@@ -29,34 +30,28 @@ public class OrderProductTest extends BaseTest {
         checkoutPage = new CheckoutPage();
         paymentPage = new PaymentPage();
         paymentDonePage = new PaymentDonePage();
-    }
 
-    @Test
-    public void UserCanOrderProducts() throws InterruptedException {
+        String path = "C:\\Users\\Natalija Mitic\\Desktop\\1\\testData.xlsx";
         driver.navigate().to("https://automationexercise.com/login");
         Assert.assertTrue(loginPage.loginForm.isDisplayed());
-        loginPage.inputLoginEmail("natalija.8@gmail.com");
-        loginPage.inputPassword("Qwerty1!");
+        loginPage.inputLoginEmail(ExcelReader.getCellValue(path,6,1));
+        loginPage.inputPassword(ExcelReader.getCellValue(path,1,2));
         loginPage.clickOnLoginButton();
-        Thread.sleep(3000);
-        Assert.assertEquals(driver.getCurrentUrl(), "https://automationexercise.com/");
-        Assert.assertTrue(homePage.loggedInName.isDisplayed());
         Assert.assertTrue(homePage.loggedInName.getText().contains("Natalija"));
         Assert.assertTrue(homePage.logoutButton.isDisplayed());
-        //closeAdPopupIfPresent1();
         homePage.clickOnProducts();
         closeAdPopupIfPresent1();
-        //Assert.assertEquals(driver.getCurrentUrl(), "https://automationexercise.com/products"); //asertacija da smo na Products page
         Assert.assertEquals(productsPage.allProducts.getText(), "ALL PRODUCTS");
         scrollToElement(productsPage.products.get(4));
-        //If you need to hover and then click something that appears after hover:
+
+        //Ako je potrebno da se zadrži kursor, a zatim klikne na nešto što se pojavljuje nakon što se zadrži kursor
         Actions actions = new Actions(driver);
         actions.moveToElement(productsPage.products.get(4)).perform();
 
-        // Now click the button that appeared after hover
+        //Sada se klikne na dugme koje se pojavilo nakon prelaska mišem preko
         wait.until(ExpectedConditions.visibilityOf(productsPage.addToCartButton));
         productsPage.addToCartButton.click();
-        Thread.sleep(3000);
+        wait.until(ExpectedConditions.visibilityOf(productsPage.addedProductMessage));
         Assert.assertTrue(productsPage.addedProductMessage.isDisplayed());
         Assert.assertTrue(productsPage.addedProductMessage.getText().contains("Your product has been added to cart"));
         productsPage.clickOnViewCart();
@@ -64,23 +59,50 @@ public class OrderProductTest extends BaseTest {
         Assert.assertTrue(shoppingCartPage.cartDesription.getText().contains("Winter Top"));
         shoppingCartPage.clickOnProceedToCheckout();
         Assert.assertEquals(driver.getCurrentUrl(), "https://automationexercise.com/checkout");
+        scrollToElement(checkoutPage.placeOrder);
         checkoutPage.clickOnPlaceOrder();
-        Thread.sleep(2000);
+        wait.until(ExpectedConditions.visibilityOf(paymentPage.nameOnCardField));
         Assert.assertEquals(driver.getCurrentUrl(), "https://automationexercise.com/payment");
-        paymentPage.inputNameOnCard("CardName");
-        paymentPage.inputCardNumber("123");
-        paymentPage.inputCVC("311");
-        paymentPage.inputExpiryMonth("05");
-        paymentPage.inputExpiryYear("2026");
+
+    }
+
+    //Test metoda koja implementira test case "Korisnik može uspešno da plati i naruci komad".
+    //Koristi se ExcelReader za povlacenje podataka iz Excel fajla "testData.xlsx".
+    //Preduslov je da je korsinik ulogovan, zatim se sa stranice All Products doda komad u korpu, nakon cega se izbrsi placanje.
+    @Test(priority = 1)
+    public void UserCanOrderProducts() throws Exception {
+        String path = "C:\\Users\\Natalija Mitic\\Desktop\\1\\testData.xlsx";
+        paymentPage.inputNameOnCard(ExcelReader.getCellValue(path,10,0));
+        paymentPage.inputCardNumber(ExcelReader.getCellValue(path,10,1));
+        paymentPage.inputCVC(ExcelReader.getCellValue(path,10,3));
+        paymentPage.inputExpiryMonth(ExcelReader.getCellValue(path,10,5));
+        paymentPage.inputExpiryYear(ExcelReader.getCellValue(path,10,6));
         scrollToElement(paymentPage.confirmOrderButton);
         wait.until(ExpectedConditions.elementToBeClickable(paymentPage.confirmOrderButton));
         paymentPage.clickOnConfirmOrder();
-        Thread.sleep(2000);
+        wait.until(ExpectedConditions.visibilityOf(paymentDonePage.paymentDoneMessage));
         Assert.assertTrue(paymentDonePage.paymentDoneMessage.getText().contains("ORDER PLACED!"));
         Assert.assertTrue(paymentDonePage.paymentDoneMessage.getText().contains("Congratulations! Your order has been confirmed!"));
-
-
     }
+
+    //Test metoda koja implementira test case "Korisnik može uspešno da plati i naruci komad sa validnim podacima iz kartice".
+    //Koristi se ExcelReader za povlacenje podataka iz Excel fajla "testData.xlsx".
+    //Test pada jer podaci iz kartice nisu validni: polja Card Number i cvc prihvataju slova,znakove..
+    @Test(priority = 2)
+    public void UserCanOrderProductWithValidCardInformation() throws Exception {
+        String path = "C:\\Users\\Natalija Mitic\\Desktop\\1\\testData.xlsx";
+        paymentPage.inputNameOnCard(ExcelReader.getCellValue(path,10,0));
+        paymentPage.inputCardNumber(ExcelReader.getCellValue(path,10,2));
+        paymentPage.inputCVC(ExcelReader.getCellValue(path,10,4));
+        paymentPage.inputExpiryMonth(ExcelReader.getCellValue(path,10,5));
+        paymentPage.inputExpiryYear(ExcelReader.getCellValue(path,10,6));
+        scrollToElement(paymentPage.confirmOrderButton);
+        wait.until(ExpectedConditions.elementToBeClickable(paymentPage.confirmOrderButton));
+        paymentPage.clickOnConfirmOrder();
+        wait.until(ExpectedConditions.visibilityOf(paymentDonePage.paymentDoneMessage));
+        Assert.assertFalse(paymentDonePage.paymentDoneMessage.getText().contains("ORDER PLACED!"));
+    }
+
 
     @AfterMethod
     public void teardown(){
